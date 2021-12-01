e84c95f197698d71295dcc0bd68b46aec55a2f7e
Travis Taylor <travis@trickfire.com>
2021-12-01 10:50:14 -0700

    Day1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -9,13 +9,48 @@ fn parse_input(input: &str) -> Vec<i64> {
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> i32 {
     let data = parse_input(input);
+    //Find descending
+    data.windows(2).filter(|x| x[0] < x[1]).count() as i32
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> i32 {
+    let data = parse_input(input);
     0
 }
 
 #[cfg(test)]
 mod tests {
-    //use super::{part1_chars as part1, part2};
 
     #[test]
-    fn example_part1() {}
+    fn example_part1() {
+        let input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        assert_eq!(super::part1(input), 7);
+    }
+
+    #[test]
+    fn example_part2() {
+        let input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        assert_eq!(super::part2(input), 5);
+    }
 }
