f849229b79b7df8e9e49ae505090cc3c5ff3e952
Travis Taylor <travis@trickfire.com>
2021-12-01 11:05:59 -0700

    Part 2 (clean up tests)

--- src/day1.rs
+++ src/day1.rs
@@ -7,24 +7,30 @@ fn parse_input(input: &str) -> Vec<i64> {
 }
 
 #[aoc(day1, part1)]
-pub fn part1(input: &str) -> i32 {
+pub fn part1(input: &str) -> usize {
     let data = parse_input(input);
     //Find descending
-    data.windows(2).filter(|x| x[0] < x[1]).count() as i32
+    data.windows(2).filter(|x| x[0] < x[1]).count()
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> i32 {
+pub fn part2(input: &str) -> usize {
     let data = parse_input(input);
-    0
+    data.windows(3)
+        .map(|x| x[0] + x[1] + x[2])
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|x| x[0] < x[1])
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn example_part1() {
-        let input = "199
+    use super::*;
+
+    fn get_test_input() -> &'static str {
+        "199
 200
 208
 210
@@ -33,24 +39,16 @@ mod tests {
 240
 269
 260
-263";
+263"
+    }
 
-        assert_eq!(super::part1(input), 7);
+    #[test]
+    fn example_part1() {
+        assert_eq!(part1(get_test_input()), 7);
     }
 
     #[test]
     fn example_part2() {
-        let input = "199
-200
-208
-210
-200
-207
-240
-269
-260
-263";
-
-        assert_eq!(super::part2(input), 5);
+        assert_eq!(part2(get_test_input()), 5);
     }
 }
