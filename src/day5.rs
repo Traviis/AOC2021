2e23048b96be7fe07b09a6ad2c9cc00b3708c58b
Travis Taylor <travis@trickfire.com>
2021-12-05 15:46:23 -0700

    Try some alternative loaders to see if I can speed it up.

--- src/day5.rs
+++ src/day5.rs
@@ -63,7 +63,7 @@ fn interpolate(consider_diags: bool, x1: u64, y1: u64, x2: u64, y2: u64) -> Vec<
 //Surprisingly, this had no real extra effect disproving my above statement
 fn interpolate_direct(
     consider_diags: bool,
-    hMap: &mut HashMap<(u64, u64), u64>,
+    h_map: &mut HashMap<(u64, u64), u64>,
     x1: u64,
     y1: u64,
     x2: u64,
@@ -76,13 +76,13 @@ fn interpolate_direct(
     if x1 == x2 {
         //Vertical
         for y in cmp::min(y1, y2)..cmp::max(y1, y2) + 1 {
-            let point = hMap.entry((x1, y)).or_insert(0);
+            let point = h_map.entry((x1, y)).or_insert(0);
             *point += 1;
         }
     } else if y1 == y2 {
         //Horizontal
         for x in cmp::min(x1, x2)..cmp::max(x1, x2) + 1 {
-            let point = hMap.entry((x, y1)).or_insert(0);
+            let point = h_map.entry((x, y1)).or_insert(0);
             *point += 1;
         }
     } else {
@@ -104,12 +104,13 @@ fn interpolate_direct(
         let b = (left_most.1 as i64) - slope * (left_most.0 as i64);
         for x in left_most.0..right_most.0 + 1 {
             let in_point = (x, (slope * (x as i64) + b) as u64);
-            let point = hMap.entry(in_point).or_insert(0);
+            let point = h_map.entry(in_point).or_insert(0);
             *point += 1;
         }
     }
 }
 
+#[allow(dead_code)]
 fn print_map(map: &HashMap<(u64, u64), u64>) {
     let (max_x, max_y) = map.iter().fold((0, 0), |(mx, ym), ((x, y), _)| {
         (if *x > mx { *x } else { mx }, if *y > ym { *y } else { ym })
@@ -128,37 +129,64 @@ fn print_map(map: &HashMap<(u64, u64), u64>) {
     }
 }
 
-fn parse_input(input: &str, consider_diags: bool) -> HashMap<(u64, u64), u64> {
+#[aoc_generator(day5, part1, vecreturn)]
+fn part1_parse_vec(input: &str) -> HashMap<(u64, u64), u64> {
+    parse_input(false, false, input)
+}
+
+#[aoc_generator(day5, part2, vecreturn)]
+fn part2_parse_vec(input: &str) -> HashMap<(u64, u64), u64> {
+    parse_input(true, false, input)
+}
+#[aoc_generator(day5, part1, direct)]
+fn part1_parse_direct(input: &str) -> HashMap<(u64, u64), u64> {
+    parse_input(false, true, input)
+}
+
+#[aoc_generator(day5, part2, direct)]
+fn part2_parse_direct(input: &str) -> HashMap<(u64, u64), u64> {
+    parse_input(true, true, input)
+}
+
+fn parse_input(consider_diags: bool, direct: bool, input: &str) -> HashMap<(u64, u64), u64> {
     let mut map = HashMap::new();
     input.split("\n").map(str::trim).for_each(|line| {
         let sp = line
             .split(&[',', ' '][..]) //Lame, Rust wants a slice
             .filter_map(|x| x.parse().ok())
             .collect::<Vec<u64>>();
-        assert!(sp.iter().count() == 4);
+        //assert!(sp.iter().count() == 4);
         //println!(" ({},{}) => ({},{})", sp[0], sp[1], sp[2], sp[3]);
-        //interpolate_direct(consider_diags, &mut map, sp[0], sp[1], sp[2], sp[3]);
-        for point in interpolate(consider_diags, sp[0], sp[1], sp[2], sp[3]) {
-            //   println!("{:?}", point);
-            let point = map.entry(point).or_insert(0);
-            *point += 1;
+        if direct {
+            interpolate_direct(consider_diags, &mut map, sp[0], sp[1], sp[2], sp[3]);
+        } else {
+            for point in interpolate(consider_diags, sp[0], sp[1], sp[2], sp[3]) {
+                //   println!("{:?}", point);
+                let point = map.entry(point).or_insert(0);
+                *point += 1;
+            }
         }
     });
 
     map
 }
 
-#[aoc(day5, part2)]
-pub fn part2(input: &str) -> u64 {
-    let map = parse_input(input, true);
-    //print_map(&map);
+#[aoc(day5, part2, vecreturn)]
+pub fn part2_vec(map: &HashMap<(u64, u64), u64>) -> u64 {
+    map.iter().filter(|(_, &v)| v > 1).count() as u64
+}
+#[aoc(day5, part2, direct)]
+pub fn part2_direct(map: &HashMap<(u64, u64), u64>) -> u64 {
     map.iter().filter(|(_, &v)| v > 1).count() as u64
 }
 
-#[aoc(day5, part1)]
-pub fn part1(input: &str) -> u64 {
-    let map = parse_input(input, false);
-    //print_map(&map);
+#[aoc(day5, part1, vecreturn)]
+pub fn part1_vec(map: &HashMap<(u64, u64), u64>) -> u64 {
+    map.iter().filter(|(_, &v)| v > 1).count() as u64
+}
+
+#[aoc(day5, part1, direct)]
+pub fn part1_direct(map: &HashMap<(u64, u64), u64>) -> u64 {
     map.iter().filter(|(_, &v)| v > 1).count() as u64
 }
 
@@ -181,12 +209,22 @@ mod tests {
     }
 
     #[test]
-    fn example_part1() {
-        assert_eq!(part1(get_test_input()), 5);
+    fn example_part1_vec() {
+        assert_eq!(part1_vec(&part1_parse_vec(get_test_input())), 5);
     }
 
     #[test]
-    fn example_part2() {
-        assert_eq!(part2(get_test_input()), 12);
+    fn example_part1_direct() {
+        assert_eq!(part1_direct(&part1_parse_direct(get_test_input())), 5);
+    }
+
+    #[test]
+    fn example_part2_vec() {
+        assert_eq!(part2_vec(&part2_parse_vec(get_test_input())), 12);
+    }
+
+    #[test]
+    fn example_part2_direct() {
+        assert_eq!(part2_direct(&part2_parse_direct(get_test_input())), 12);
     }
 }
