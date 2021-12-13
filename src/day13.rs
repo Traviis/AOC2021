010fc389e1475d0a476803f6f54031aed68f98df
Travis Taylor <travis@trickfire.com>
2021-12-13 12:16:39 -0700

    Set up parsing don't have time to test.

--- src/day13.rs
+++ src/day13.rs
@@ -1,13 +1,47 @@
-//use std::collections::HashMap;
+use std::collections::HashSet;
+
+#[derive(Debug)]
+enum Instruction {
+    FoldX(i32),
+    FoldY(i32),
+}
 
 #[aoc_generator(day13)]
-fn day13_parse(input: &str) -> &str {
-    ""
+fn day13_parse(input: &str) -> (HashSet<(i32, i32)>, Vec<Instruction>) {
+    let mut inp = input.split("\n\n");
+    let dots = inp
+        .next()
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let mut spl = line.split(",").flat_map(str::parse::<i32>);
+            (spl.next().unwrap(), spl.next().unwrap())
+        })
+        .collect::<HashSet<(i32, i32)>>();
+
+    let inst = inp
+        .next()
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let v = line.split(" ").last().unwrap();
+            let mut axis_set = v.split("=");
+            let axis = axis_set.next().unwrap();
+            let val = axis_set.next().unwrap().parse::<i32>().unwrap();
+            match axis {
+                "y" => Instruction::FoldY(val),
+                "x" => Instruction::FoldX(val),
+                _ => panic!("Bad axis"),
+            }
+        })
+        .collect::<Vec<_>>();
+
+    (dots, inst)
 }
 
 #[aoc(day13, part1)]
-pub fn day13_part1(map: &str) -> u128 {
-    0
+pub fn day13_part1() -> u128 {
+    println!("{:?} => {:?}", 
 }
 #[aoc(day13, part2)]
 pub fn day13_part2(map: &str) -> u128 {
